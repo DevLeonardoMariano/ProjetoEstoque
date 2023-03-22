43e950712d8b5a777b7cc2dc7b57d2f2bb4cec50
DevLeonardoMariano <leonardomariano02@outlook.com>
2023-03-22 19:28:08 -0300

    Criado index e cotroller
    Implementado alguns metodos na categoria controller e criado produto controller com os mesmo metodo e para finalizar criado index de cadastrar e listar de produto

--- src/main/java/br/edu/toledoprudente/Controller/ProdutoController.java
+++ src/main/java/br/edu/toledoprudente/Controller/ProdutoController.java
@@ -1,30 +1,36 @@
 package br.edu.toledoprudente.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import br.edu.toledoprudente.Entity.Categoria;
+import br.edu.toledoprudente.Entity.Produto;
 import br.edu.toledoprudente.Repository.CategoriaRepository;
+import br.edu.toledoprudente.Repository.ProdutoRepository;
 
 @Controller
-@RequestMapping("/categoria")
-public class CategoriaController {
+@RequestMapping("/produto")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository repository;
 	
 	@Autowired
-	private CategoriaRepository repository;
+	private CategoriaRepository repositorycategoria;
 	
 	//tag novo utilizado na URL
 	@GetMapping("/novo")
 	public String novo(ModelMap model) {
-		model.addAttribute("categoria", new Categoria());
-		return "/categoria/index";
+		model.addAttribute("produto", new Produto());
+		return "/produto/index";
 	}
 	
 //-------------------------------------- metodo listar ------------------------------------------	
@@ -32,16 +38,16 @@ public class CategoriaController {
 	public String listar(ModelMap model) {
 		model.addAttribute("lista",repository.findAll());
 		
-		return "/categoria/listar";
+		return "/produto/listar";
 	}
 	
 //-------------------------------------- metodo pre alterar ------------------------------------------		
 	@GetMapping("/prealterar")
 	public String preAlterar(@RequestParam(name="id") int id,ModelMap model) {
 		
-		model.addAttribute("categoria", repository.findById(id));
+		model.addAttribute("produto", repository.findById(id));
 		
-		return "/categoria/index";
+		return "/produto/index";
 	}
 
 	
@@ -60,20 +66,20 @@ public class CategoriaController {
 			
 		}
 		model.addAttribute("lista", repository.findAll());
-		return "/categoria/listar";
+		return "/produto/listar";
 	}
 	
 	
 //-------------------------------------- metodo salvar ------------------------------------------	
 	@PostMapping("/salvar")
-	public String salvar(@ModelAttribute("categoria") Categoria cat, ModelMap model) {
+	public String salvar(@ModelAttribute("produto") Produto pro, ModelMap model) {
 		try {
 			
-			if(cat.getId()== null)
-				repository.save(cat);
+			if(pro.getId()== null)
+				repository.save(pro);
 			
 			else
-				repository.update(cat);
+				repository.update(pro);
 			model.addAttribute("mensagem", "Salvo com sucesso");
 			model.addAttribute("retorno", true);
 			
@@ -82,9 +88,13 @@ public class CategoriaController {
 			model.addAttribute("retorno", false);
 		}
 		
-		return "/categoria/index";
+		return "/produto/index";
 	}
-	
-	
+	/*metodo usado para retornar dados para selection*/
+	@ModelAttribute(name="listacategoria")
+	public List<Categoria> listaCategoria(){
+		
+		return repositorycategoria.findAll();
+	}
 	
 }
